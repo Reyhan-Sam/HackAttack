7fc40cbf23533becf321a09cba6653dcae670af0
Reyhan-Sam <reyhansam13@gmail.com>
2024-03-01 20:55:24 -0800

    changes to app.py added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-print("stuff")
-
-from flask import Flask
+import torch    
+import cv2
+import numpy as np
+import tensorflow as tf
+from flask import Flask, rendor_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask!"  
