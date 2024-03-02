311e3a5f975157d6ec9ad5c87a9487bcdb46fcaf
Reyhan-Sam <reyhansam13@gmail.com>
2024-03-02 00:28:01 -0800

    added box and text with the video

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-import torch    
-import cv2
-import numpy as np
-import tensorflow as tf
-from flask import Flask, rendor_template
+from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
